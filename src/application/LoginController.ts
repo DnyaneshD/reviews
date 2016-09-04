8ba3ebdeb4d1d27b890096a29a35f169208ec826
Dnyanesh Deshpande <dnyan.desh@gmail.com>
2016-09-04 23:06:16 +0530

    'token'

--- src/application/LoginController.ts
+++ src/application/LoginController.ts
@@ -1,10 +1,26 @@
+/// <reference path="../../typings/typings.d.ts" />
 
-export class LoginController{
-    
-    login(userName,password){
-        
-        if(userName == "test" && password === "123"){
-          return "You are valid user";   
-        }
+import * as jsonWebToken from "jsonwebtoken";
+
+export class LoginController {
+
+    //Login will be implemented when Login and Password saved in DB
+    login(userName, password) {
+
+    }
+
+    //OAuth token will be exchanged and login token will be created 
+    loginOAuth() {
+
+        let accessToken = jsonWebToken.sign({}, "xyz1122", {
+            expiresIn: 3600 //set expirty of token 
+        });
+
+        let response = {
+            "accessSucess": true,
+            "accessToken": accessToken
+        };
+
+        return response;
     }
 } 
--- src/server.ts
+++ src/server.ts
@@ -4,34 +4,23 @@ import * as express from "express";
 import { LoginController } from './application/LoginController';
 import * as bodyParser from "body-parser";
 import * as jsonWebToken from "jsonwebtoken";
+
 let app = express();
 let loginContoller = new LoginController();
 
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
-app.set('jwtSecret', "xyz1122")
-
 //Login route
 app.route('/login')
+    // TODO: Remove if not required
     // show the form (GET http://localhost:8080/login)
     .get(function(req, res) {
-
-        let accessToken = jsonWebToken.sign({}, app.get('jwtSecret'), {
-                    //Set the expiration
-                    expiresIn: 3600 //we are setting the expiration time of 1 hr. 
-                });
-
-       let response = {
-           "accessSucess": true,
-           "accessToken": accessToken
-       }; 
-
-        res.send(response);
     })
 
     // process the form (POST http://localhost:8080/login)
     .post(function(req, res) {
+        
         let result = loginContoller.login(req.body.userName, req.body.password);
         res.send(result);
     });
