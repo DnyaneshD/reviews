89bec3ba2c53a5403e9f8fbb5bb57b78708c17aa
Dnyanesh Deshpande <dnyanesh88@hotmail.com>
2016-09-06 22:22:02 +0530

    Added CORS
    CORS

--- src/server.ts
+++ src/server.ts
@@ -8,6 +8,12 @@ import * as jsonWebToken from "jsonwebtoken";
 let app = express();
 let loginContoller = new LoginController();
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
@@ -16,13 +22,19 @@ app.route('/login')
     // TODO: Remove if not required
     // show the form (GET http://localhost:8080/login)
     .get(function(req, res) {
+        res.send('Nothing Nothing to share');
     })
 
     // process the form (POST http://localhost:8080/login)
     .post(function(req, res) {
-        
-        let result = loginContoller.login(req.body.userName, req.body.password);
-        res.send(result);
+        if(req.body.userName && req.body.password){
+            let result = loginContoller.login(req.body.userName, req.body.password);
+            res.send(result);
+        }
+        if(req.body.profileId){
+            let token = loginContoller.loginOAuth();
+            res.send(token);
+        }
     });
 
 app.listen(3000);
